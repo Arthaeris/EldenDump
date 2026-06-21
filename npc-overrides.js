d224f7fdc200627488b94e6dee921df84575d029
Arthaeris <the.shou7@googlemail.com>
2026-06-21 02:35:50 +0200

    Update npc-overrides.js

--- npc-overrides.js
+++ npc-overrides.js
@@ -1,1 +1,17 @@
+const MANUAL_NPC_TALK_MAPPINGS = {
+  '1001': ['Melina', 'メリナ'],
+  '1002': ['Narrator', 'Narrator'],
+  '1003': ['Narrator', 'Narrator'],
+  '1004': ['Narrator', 'Narrator'],
+  '1005': ['Narrator', 'Narrator'],
+  '1006': ['Narrator', 'Narrator'],
 
+  '1010': ['Finger Reader Crone', '指読みの老婆'],
+
+  'd1010': ['Messmer the Impaler', '串刺し公、メスメル'],
+  'd1021': ['Hornsent Grandam', '角人の老婆'],
+  'd1031': ['Igon', 'エーゴン'],
+  'd1032': ['Igon', 'エーゴン'],
+  'd1040': ['Midra, Lord of Frenzied Flame', '狂い火の王、ミドラー'],
+  'd1051': ['Moore', 'ムーア']
+};
