9d52f5ab2a9a82752653e76bef7006011b66d9fa
Arthaeris <the.shou7@googlemail.com>
2026-06-24 00:05:29 +0100

    Update build-info.js

--- build-info.js
+++ build-info.js
@@ -1,3 +1,16 @@
 const BUILD_INFO = {
-  updatedAt: "2026-06-23T00:00:00Z"
+  version: '1.2.7',
+
+  updatedAt: '2026-06-23T16:00:00Z',
+
+  announcement: {
+    enabled: true,
+
+    title: 'Recent Update',
+
+    message:
+      'Dialogue grouping was rewritten. Some NPC pages may behave unexpectedly while testing continues.',
+
+    showForHours: 48
+  }
 };
