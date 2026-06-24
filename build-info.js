d6391ef9976fdd7ad8d3fdc90ae4676b0d9c8b5a
Arthaeris <the.shou7@googlemail.com>
2026-06-24 12:37:36 +0100

    Update build-info.js

--- build-info.js
+++ build-info.js
@@ -1,6 +1,18 @@
 const BUILD_INFO = {
   
   announcements: [
+    
+    
+    {
+      version: '0.5.3',
+      updatedAt: '2026-06-24T08:00:00Z',
+      enabled: true,
+      title: 'Translate Button',
+      message:
+        'When displaying entries in Japanese, a 'Translate' button now appears that can be pressed for an insert of the selected text into the translator service DeepL.',
+      showForHours: 72
+    },
+    
     {
       version: '0.5.2',
       updatedAt: '2026-06-23T18:00:00Z',
