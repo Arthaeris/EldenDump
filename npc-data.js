3ec621ecf5c917ec907f2b72fe1cf22e6ccb6d0a
Arthaeris <the.shou7@googlemail.com>
2026-06-24 02:14:55 +0100

    Create npc-data.js

---
+++ npc-data.js
@@ -0,0 +1,45 @@
+const NPC_METADATA = {
+  "Aureliette": {
+    image: "images/aureliette.webp",
+
+    trivia: [
+      "One of the jellyfish sisters.",
+      "Can be reunited with her sister at Stargazer's Ruins."
+    ],
+
+    notes: [
+      "Uses dialogue ID 2270."
+    ],
+
+    relatedNpcs: [
+      "Jellyfish",
+      "Roderika"
+    ],
+
+    relatedItems: [
+      "Spirit Jellyfish Ashes"
+    ]
+  },
+
+  "Ranni the Witch": {
+    image: "images/ranni.webp",
+
+    trivia: [
+      "Appears under multiple dialogue IDs.",
+      "Central character of the Age of Stars ending."
+    ],
+
+    relatedNpcs: [
+      "Renna",
+      "Blaidd the Half-Wolf",
+      "War Counselor Iji",
+      "Seluvis"
+    ],
+
+    relatedItems: [
+      "Dark Moon Ring",
+      "Miniature Ranni",
+      "Discarded Palace Key"
+    ]
+  }
+};
