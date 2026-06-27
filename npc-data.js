f392dc8d51824dd85495843d02285c89c9ae2e62
Arthaeris <the.shou7@googlemail.com>
2026-06-27 05:53:43 +0100

    Update npc-data.js

--- npc-data.js
+++ npc-data.js
@@ -57,11 +57,11 @@ const NPC_MENTION_ALIASES = {
     "Ymir",
     "Count Ymir"
   ],
-  
+
   "Alexander, Warrior Jar": [
+    "Alexander",
     "Iron Fist",
-    "Iron Fist Alexander",
-    "Alexander"
+    "Iron Fist Alexander"
   ],
 
   "Blaidd the Half-Wolf": [
@@ -78,5 +78,185 @@ const NPC_MENTION_ALIASES = {
   "Sir Ansbach": [
     "Ansbach",
     "Pureblood Knight Ansbach"
+  ],
+
+  "Margit, the Fell Omen": [
+    "Margit",
+    "Fell Omen"
+  ],
+
+  "Morgott, the Omen King": [
+    "Morgott",
+    "Omen King"
+  ],
+
+  "Godrick the Grafted": [
+    "Godrick",
+    "the Grafted"
+  ],
+
+  "Rennala, Queen of the Full Moon": [
+    "Rennala",
+    "Queen of the Full Moon"
+  ],
+
+  "Starscourge Radahn": [
+    "Radahn",
+    "Starscourge",
+    "General Radahn"
+  ],
+
+  "Malenia, Blade of Miquella": [
+    "Malenia",
+    "Blade of Miquella"
+  ],
+
+  "Miquella": [
+    "Miquella",
+    "Kind Miquella",
+    "Kindly Miquella"
+  ],
+
+  "Mohg, Lord of Blood": [
+    "Mohg",
+    "Lord of Blood",
+    "Lord Mohg"
+  ],
+
+  "Rykard, Lord of Blasphemy": [
+    "Rykard",
+    "Lord of Blasphemy"
+  ],
+
+  "Dragonlord Placidusax": [
+    "Placidusax",
+    "Dragonlord"
+  ],
+
+  "Maliketh, the Black Blade": [
+    "Maliketh",
+    "Black Blade"
+  ],
+
+  "Godfrey, First Elden Lord": [
+    "Godfrey",
+    "First Elden Lord"
+  ],
+
+  "Hoarah Loux, Warrior": [
+    "Hoarah Loux"
+  ],
+
+  "Radagon of the Golden Order": [
+    "Radagon"
+  ],
+
+  "Elden Beast": [],
+
+  "Fire Giant": [],
+
+  "Godskin Apostle": [
+    "Apostle"
+  ],
+
+  "Godskin Noble": [
+    "Noble"
+  ],
+
+  "Astel, Naturalborn of the Void": [
+    "Astel",
+    "Naturalborn of the Void"
+  ],
+
+  "Ancestor Spirit": [],
+
+  "Regal Ancestor Spirit": [
+    "Ancestor Spirit"
+  ],
+
+  "Lichdragon Fortissax": [
+    "Fortissax"
+  ],
+
+  "Commander Niall": [
+    "Niall"
+  ],
+
+  "Commander O'Neil": [
+    "O'Neil"
+  ],
+
+  "Elemer of the Briar": [
+    "Elemer"
+  ],
+
+  "Royal Knight Loretta": [
+    "Royal Knight Loretta"
+  ],
+
+  "Loretta, Knight of the Haligtree": [
+    "Loretta",
+    "Knight of the Haligtree"
+  ],
+
+  "God-Devouring Serpent": [
+    "Serpent",
+    "God-Devouring Serpent"
+  ],
+
+  // Shadow of the Erdtree
+
+  "Divine Beast Dancing Lion": [
+    "Dancing Lion",
+    "Divine Beast"
+  ],
+
+  "Rellana, Twin Moon Knight": [
+    "Rellana",
+    "Twin Moon Knight"
+  ],
+
+  "Messmer the Impaler": [
+    "Messmer",
+    "the Impaler"
+  ],
+
+  "Putrescent Knight": [],
+
+  "Romina, Saint of the Bud": [
+    "Romina",
+    "Saint of the Bud"
+  ],
+
+  "Bayle the Dread": [
+    "Bayle",
+    "the Dread"
+  ],
+
+  "Midra, Lord of Frenzied Flame": [
+    "Midra",
+    "Lord of Frenzied Flame"
+  ],
+
+  "Commander Gaius": [
+    "Gaius",
+    "General Gaius"
+  ],
+
+  "Scadutree Avatar": [],
+
+  "Metyr, Mother of Fingers": [
+    "Metyr",
+    "Mother of Fingers"
+  ],
+
+  "Needle Knight Leda": [
+    "Leda"
+  ],
+
+  "Promised Consort Radahn": [
+    "Consort Radahn",
+    "Promised Consort",
+    "Promised Consort Radahn"
   ]
 };
